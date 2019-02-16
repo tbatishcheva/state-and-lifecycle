65994b2c53069501bae47c1b9ab24514306351b7
TatBat <tat.batishcheva@gmail.com>
2019-02-16 14:03:07 +0300

    Add Lifecycle Methods to a Class.

--- src/index.js
+++ src/index.js
@@ -5,27 +5,27 @@ import * as serviceWorker from './serviceWorker';
 
 class Clock extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-      }
-
-      componentDidMount() {
-        this.timerID = setInterval(
-          () => this.tick(),
-          1000
-        );
-      }
-
-      componentWillUnmount() {
-        clearInterval(this.timerID);
-      }
-
-      tick() {
-        this.setState({
-          date: new Date()
-        });
-      }
-      
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
     render() {
       return (
         <div>
@@ -36,15 +36,11 @@ class Clock extends React.Component {
     }
   }
   
-  function tick() {
-    ReactDOM.render(
-      <Clock />,
-      document.getElementById('root')
-    );
-  }
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
   
-  setInterval(tick, 1000);
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
