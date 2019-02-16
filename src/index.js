5e421ae24bf09150c51b7bbc462504ff2d2ee8eb
TatBat <tat.batishcheva@gmail.com>
2019-02-16 13:55:26 +0300

    Add Lifecycle Methods to a Class.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,33 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+      }
+
+      componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000
+        );
+      }
+
+      componentWillUnmount() {
+        clearInterval(this.timerID);
+      }
+
+      tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+      
     render() {
       return (
         <div>
           <h1>Hello, world!</h1>
-          <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         </div>
       );
     }
@@ -16,7 +38,7 @@ class Clock extends React.Component {
   
   function tick() {
     ReactDOM.render(
-      <Clock date={new Date()} />,
+      <Clock />,
       document.getElementById('root')
     );
   }
